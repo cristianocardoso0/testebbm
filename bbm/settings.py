1d23c7248f1179ab4c0ca071b98b52718fab7a9e
Cristiano Cardoso <cristiano@minhai9.com.br>
2022-06-06 17:35:21 -0300

    change to postgresql

--- bbm/settings.py
+++ bbm/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'bbm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgrespw',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
